4d898dbf2ac817e0564dee153074549c049b422e
James Dimacali <frostbitezebra421@gmail.com>
2023-09-02 21:02:37 -0700

    converted all files to typescript

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,14 @@
-import { View, Text } from "react-native";
+import { useState } from "react";
+import { View, ScrollView, SafeAreaView, Text } from "react-native";
+import { Stack, useRouter } from "expo-router";
+
+import { COLORS, icons, images, SIZES } from "../constants";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
 
 const Home = () => {
   return (

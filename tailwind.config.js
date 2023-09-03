2ca1776d875a64aae5234d9befc5df3db6dc11a1
James Dimacali <frostbitezebra421@gmail.com>
2023-09-02 20:55:18 -0700

    fixed tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   content: [
     "./app/**/*.{js,jsx,ts,tsx}",
-    "./<custom directory>/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
